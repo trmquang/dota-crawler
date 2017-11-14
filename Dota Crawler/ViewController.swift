78d23c17a2a06cb83ac080f787b65cf8b44be747
trmquang <trmquang0109@gmail.com>
2017-11-14 19:55:10 +0700

    Initial commit

---
+++ Dota Crawler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dota Crawler
+//
+//  Created by Quang Minh Trinh on 11/14/17.
+//  Copyright © 2017 Quang Minh Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
